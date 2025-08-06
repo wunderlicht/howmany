28fb414372389d9b5b33482178aaa485d37ee5a9
wunderlicht <wunderlich@actualchange.eu>
2025-08-06 23:45:52 +0200

    add single scenario

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func main() {
+	hd := []int{2, 6, 1, 3, 4, 8}
+	fmt.Println(scenario(hd, 6))
+}
+
+// scenario picks randomly from the historical data. How many picks are necessary to match the target?
+// Example
+// historical = [2, 4, 3, 2], target = 10
+// possible picks: 4, 3, 2, 3	summed up: 12, needed iterations: 4
+// possible picks: 4, 3, 3		summed up: 10, needed iterations: 3
+func scenario(historicalData []int, target int) (iterations int) {
+	sum := 0
+	for iterations = 0; sum < target; iterations++ {
+		pick := rand.IntN(len(historicalData))
+		sum += historicalData[pick]
+	}
+	return
+}
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func Test_scenario(t *testing.T) {
+	type args struct {
+		historicalData []int
+		target         int
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantIterations int
+	}{
+		{"should be done in one iteration", args{[]int{2, 2, 2}, 2}, 1},
+		{"should be done in two iteration", args{[]int{2, 2, 2}, 4}, 2},
+		{"should be done in three iteration", args{[]int{3, 2, 2}, 6}, 3},
+		{"one element", args{[]int{3}, 12}, 4},
+		// Add more test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotIterations := scenario(tt.args.historicalData, tt.args.target); gotIterations != tt.wantIterations {
+				t.Errorf("scenario() = %v, want %v", gotIterations, tt.wantIterations)
+			}
+		})
+	}
+}
+
+func Test_scenario_should_panic_on_empty_historicData(t *testing.T) {
+	defer func() {
+		_ = recover()
+	}()
+	_ = scenario([]int{}, 4) //this should panic
+	// If there was no panic the test will fail
+	t.Errorf("scenario() with empty historic data should have paniced but didn't")
+}
