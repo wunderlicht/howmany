b6f9c7020df33ab1d184f3d62296a496e0599221
wunderlicht <wunderlich@actualchange.eu>
2025-08-11 00:52:26 +0200

    add read CSV and tests

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"math/rand/v2"
+	"strconv"
 	"strings"
 )
 
@@ -44,6 +47,27 @@ func runSimulation(historicalData []int, target, scenarios int) (occurrences map
 		occurrences[scenario(historicalData, target)]++
 	}
 	return
+}
+
+// returns an array of data read from the CVS. The reader is an open file.
+func readHistoryCSV(r io.Reader) (history []int, err error) {
+
+	history = make([]int, 0, 100) //allocate a bigger chunk to minimize reallocation
+	rows, err := csv.NewReader(r).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 1; i < len(rows); i++ {
+		const donePos = 1
+		doneStr := rows[i][donePos]
+		done, err := strconv.Atoi(doneStr)
+		if err != nil {
+			return nil, err
+		}
+		history = append(history, done)
+	}
+	return history, nil
 }
 
 // formats the results for output. Changes to the appearance are made here.
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"io"
 	"reflect"
 	"strings"
 	"testing"
@@ -146,6 +148,56 @@ func Test_runSimulation(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotOccurrences := runSimulation(tt.args.historicalData, tt.args.target, tt.args.scenarios); !reflect.DeepEqual(gotOccurrences, tt.occurrences) {
 				t.Errorf("runSimulation() = %v, want %v", gotOccurrences, tt.occurrences)
+			}
+		})
+	}
+}
+
+// scaffold to force a read error
+type errReader struct{}
+
+func (e errReader) Read(_ []byte) (n int, err error) {
+	return 0, errors.New("forced read error")
+}
+
+func Test_readHistoryCSV(t *testing.T) {
+
+	tests := []struct {
+		name        string
+		r           io.Reader
+		wantHistory []int
+		wantErr     bool
+	}{
+		{"header only should return empty array",
+			strings.NewReader("iteration, completed"),
+			[]int{},
+			false},
+		{"done value not an integer should throw an error",
+			strings.NewReader("a,b\n1,zwei"),
+			nil,
+			true},
+		{"one data row should return one value",
+			strings.NewReader("#iteration,done\na,1"),
+			[]int{1},
+			false},
+		{"zwo data row should return two values",
+			strings.NewReader("#iteration,done\na,1\nb,2"),
+			[]int{1, 2},
+			false},
+		{"reader error should throw an error",
+			errReader{},
+			nil,
+			true},
+		// Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotHistory, err := readHistoryCSV(tt.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("readHistoryCSV() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(gotHistory, tt.wantHistory) {
+				t.Errorf("readHistoryCSV() gotHistory = %v, want %v", gotHistory, tt.wantHistory)
 			}
 		})
 	}
